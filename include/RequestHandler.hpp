02f109d12a1a7f0d47c1ff00ec2e92c9effd5b9e
赫英博 <heyingbo@heyingbodeMacBook-Air.local>
2026-08-13 11:15:07 +0200

    handle get

--- include/RequestHandler.hpp
+++ include/RequestHandler.hpp
@@ -3,9 +3,13 @@
 
 # include "HttpRequest.hpp"
 # include "HttpResponse.hpp"
+# include <string>
 
 class RequestHandler
 {
+    HttpResponse handleGet(const HttpRequest &request, const std::string &root);
+    // HttpResponse handlePost(const HttpRequest &request);
+    // HttpResponse handleDelete(const HttpRequest &request);
 public:
     RequestHandler();
     ~RequestHandler();
--- src/RequestHandler.cpp
+++ src/RequestHandler.cpp
@@ -1,5 +1,8 @@
 #include "RequestHandler.hpp"
 #include "HttpStatus.hpp"
+#include <fstream>
+#include <sstream>
+#include <sys/stat.h>
 
 RequestHandler::RequestHandler()
 {}
@@ -7,36 +10,63 @@ RequestHandler::RequestHandler()
 RequestHandler::~RequestHandler()
 {}
 
-HttpResponse RequestHandler::handle(const HttpRequest &request)
+HttpResponse RequestHandler::handleGet(const HttpRequest &request, const std::string &root)
 {
     HttpResponse response;
-    if (request.getMethod() == "GET")
+    std::string path = root + request.getPath();
+    struct stat info;
+    if (stat(path.c_str(), &info) == -1)
     {
-        if (request.getPath() == "/")
-        {
-            response.setStatus(HTTP_OK);
-            response.setHeader("Content-Type", "text/plain");
-            response.setHeader("Connection", "close");
-            response.setBody("Hello World");   
-        }
-        else
-        {
-            response.setStatus(HTTP_NOT_FOUND);
-            response.setHeader("Content-Type", "text/plain");
-            response.setHeader("Connection", "close");
-            response.setBody("404 Not Found");
-        }
+        response.setStatus(HTTP_NOT_FOUND);
+        response.setHeader("Content-Type", "text/plain");
+        response.setHeader("Connection", "close");
+        response.setBody("404 Not Found");
+        return response;
     }
+    if (S_ISDIR(info.st_mode))
+    {
+        if (!path.empty() && path[path.size() - 1] != '/')
+            path += "/";
+        path += "index.html";
+    }
+    else if (!S_ISREG(info.st_mode))
+    {
+        response.setStatus(HTTP_FORBIDDEN);
+        response.setHeader("Content-Type", "text/plain");
+        response.setHeader("Connection", "close");
+        response.setBody("403 Forbidden");
+        return response;
+    }
+    std::ifstream file(path.c_str());
+    if (!file.is_open())
+    {
+        response.setStatus(HTTP_FORBIDDEN);
+        response.setHeader("Content-Type", "text/plain");
+        response.setHeader("Connection", "close");
+        response.setBody("403 Forbidden");
+        return response;
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    response.setStatus(HTTP_OK);
+    response.setHeader("Content-Type", "text/plain");
+    response.setHeader("Connection", "close");
+    response.setBody(buffer.str());
+    return response;
+}
+
+HttpResponse RequestHandler::handle(const HttpRequest &request)
+{
+    if (request.getMethod() == "GET")
+        return handleGet(request, "./www");
     // else if (request.getMethod() == "POST")
     // {}
     // else if (request.getMethod() == "DELETE")
     // {}
-    else
-    {
-        response.setStatus(HTTP_METHOD_NOT_ALLOWED);
-        response.setHeader("Content-Type", "text/plain");
-        response.setHeader("Connection", "close");
-        response.setBody("405 Method Not Allowed");
-    }
+    HttpResponse response;
+    response.setStatus(HTTP_METHOD_NOT_ALLOWED);
+    response.setHeader("Content-Type", "text/plain");
+    response.setHeader("Connection", "close");
+    response.setBody("405 Method Not Allowed");
     return response;
 }
